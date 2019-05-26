5f9de5bdd6d606255472fc68eaa6d3ad7ca9c8ad
Márcia Teixeira <marcia309@hotmail.com>
2019-05-26 21:30:57 +0100

    Fixed gatilho2

--- gatilho2_adiciona.sql
+++ gatilho2_adiciona.sql
@@ -2,10 +2,12 @@ DROP TRIGGER IF EXISTS HoraConsultas;
 
 CREATE TRIGGER HoraConsultas
 BEFORE INSERT ON Consulta
-WHEN (NEW.horaInicio IN 
+WHEN NEW.horaInicio IN 
 (SELECT horaInicio FROM Consulta
-	WHERE idMedico = (SELECT horaInicio FROM Consulta WHERE idMedico = NEW.idMedico AND data_consulta = NEW.data_consulta)))
-OR (NEW.horaInicio > (SELECT horaFim FROM HorarioAtendimento WHERE idMedico = NEW.idMedico AND dataAtend = NEW.data_consulta))
+ WHERE (idMedico = NEW.idMedico AND data_consulta = NEW.data_consulta))
+OR
+NEW.horaInicio > (SELECT horaFim FROM HorarioAtendimento 
+	WHERE idMed = NEW.idMedico AND dataAtend = NEW.data_consulta)
 BEGIN
-SELECT raise(rollback, 'Hora de consulta coincidente com outra consulta do mesmo medico');
+SELECT raise(rollback, 'Hora de Consulta indisponivel');
 END
